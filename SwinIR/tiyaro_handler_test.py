144638649b797e7d54306889de215c3fe621b12b
Venkat2811 <venkat@tiyaro.ai>
2022-04-27 21:57:10 +0000

    test enhancements

--- SwinIR/tiyaro_handler_test.py
+++ SwinIR/tiyaro_handler_test.py
@@ -1,69 +1,21 @@
-from tiyaro_handler.model_handler import TiyaroHandler
-
-import os
-import sys
 import json
-import yaml
+import sys
 import traceback
-import validators
+
 import requests
-import shutil
+import validators
+import yaml
+from tiyaro.sdk.test_utils.util import get_input_json, get_pretrained_file_path, save_test_input, save_test_output
 
-FROM_CONFIG = 'from-config'
-MODEL_CONFIG = './tiyaro_handler/model_manifest.yml'
-FIELD = 'pretrained_model_url'
-
-
-def get_pretrained_file_path(x):
-    def local_file(x, err_msg):
-        if os.path.isfile(x):
-            return x
-        else:
-            raise ValueError(err_msg)
-
-    if x == FROM_CONFIG:
-        # read from yaml
-        # if url, download to /tmp and return path
-        # if local path, return path
-        with open(MODEL_CONFIG, 'r') as file:
-            contents = yaml.safe_load(file)
-            value = contents[FIELD]
-            if (
-                (value is None) 
-                or (not isinstance(value, str)) 
-                or (not value.strip())
-                ):
-                raise ValueError(f'Please set a valid model {FIELD} in {MODEL_CONFIG}')
-            if validators.url(value):
-                r = requests.get(value, stream = True)
-                file_path = '/tmp/pre_trained.pth'
-                print(f'DOWNLOADING - pretrained file to {file_path} from URL: {value}')
-                if r.status_code == 200:
-                    with open(file_path, 'wb') as f:
-                        r.raw.decode_content = True
-                        shutil.copyfileobj(r.raw, f)
-                    print('DOWNLOAD - Done')
-                    return file_path
-                else:
-                    raise RuntimeError(f'Unable to download pretrained file from: {value}')
-            else:
-                return local_file(value, 'Expected valid local file path')
-
-    return local_file(x, 'Expected valid local file path')
-
-def get_input_json(x):
-    if os.path.isfile(x):
-        with open(x, 'r') as f:
-            return json.load(f)
-    if isinstance(x, str):
-        return json.loads(x)
-    
-    raise ValueError('Expected Valid JSON input string or file path')
+from tiyaro_handler.model_handler import TiyaroHandler
 
 if __name__ == "__main__":
     try:
         file_path = get_pretrained_file_path(sys.argv[1])
         input_json = get_input_json(sys.argv[2])
+        output_file = sys.argv[3]
+        if output_file == "None":
+            output_file = None
 
         ob = TiyaroHandler()
         ob.setup_model(pretrained_file_path=file_path)
@@ -72,6 +24,7 @@ if __name__ == "__main__":
 
         if ob.inputSchema:
             ob.inputSchema().load(input_json)
+            save_test_input(input_json)
             print(f'INPUT - Validation Done')
         else:
             print('WARN - Input schema not defined')
@@ -79,16 +32,24 @@ if __name__ == "__main__":
         print(f'INFERENCE - Started')
         output = ob.infer(json_input=input_json)
         print(f'INFERENCE - Done')
-
+        
         if ob.outputSchema:
             output = ob.outputSchema().load(output)
+            save_test_output(output)
             print(f'OUTPUT - Validation Done')
         else:
             print('WARN - Output schema not defined')
+        
 
         print('OUTPUT STARTS - {}'.format('*'*50))
         print(json.dumps(output, indent=4, sort_keys=True))
         print('OUTPUT ENDS - {}'.format('*'*50))
+
+        if output_file:
+            if not ".json" in output_file:
+                output_file += ".json"
+            with open(output_file, 'w+', encoding='utf-8') as f:
+                json.dump(output, f, ensure_ascii=False, indent=4)
 
     except Exception as e:
         print(f'ERROR - {e}')
